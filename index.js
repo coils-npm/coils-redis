5e45e60f6599aa0eaa76a280ea8ce2e2e7dddf75
mars <mars@mars.com>
2018-09-10 16:47:34 +0800

    add lockWithKey

--- index.js
+++ index.js
@@ -5,17 +5,48 @@ module.exports = {
 	mounted (application, options) {
 		const client = redis.createClient(options)
 		client.on('connect', () => { console.log('redis connected') })
-		const clientProxy = new Proxy(client, {
+		let clientEx = Object.assign(client, {
+			async lockWithKey (key, cb, {currentTry = 1, maxTry = 3, tryInterval = 1000, expire = 5000} = options = {}) {
+				if (await this['setnx'](key, 'auto-lock')) {
+					await this['expire'](key, expire / 1000)
+					let result = null
+					try {
+						result = await cb()
+					} catch (err) {
+						throw (err)
+					} finally {
+						await this['del'](key)
+					}
+					return Promise.resolve(result)
+				} else {
+					return new Promise((resolve, reject) => {
+						setTimeout(() => {
+							if (currentTry++ > maxTry) {
+								return reject(new Error('Too busy get Lock, Try Later Please!'))
+							}
+							return resolve(this.lockWithKey(key,cb, Object.assign(options, {currentTry})))
+						}, tryInterval)
+					})
+				}
+			}
+		})
+		const clientProxy = new Proxy(clientEx, {
 			get (target, prop) {
 				if(target[prop]) {
-					return promisify(target[prop]).bind(target);
+					if (['lockWithKey'].indexOf(prop) !== -1) {
+						return target[prop]
+					} else {
+						return promisify(target[prop]).bind(target);
+					}
 				} else {
 					throw new Error(`redis proxy can not find the method ${prop}`)
 				}
 			}
 		})
 		Object.defineProperties(application, {
-			'_redis': { "get": () => { return clientProxy } }
+			'_redis': { "get": () => {
+				return clientProxy
+			}}
 		})
 	}
 }
--- test/index.js
+++ test/index.js
@@ -10,3 +10,32 @@ describe("test base", function () {
 		assert( value === 'world', 'set fail')
 	})
 })
+
+describe("test setnx", function () {
+	it("setnx, should success", async () => {
+		await application._redis.del('setnx')
+		let value = await application._redis.setnx('setnx', 'world')
+		assert( value === 1, 'set fail')
+		value = await application._redis.setnx('setnx', 'world')
+		assert( value === 0, 'set fail')
+	})
+})
+
+describe("test withLock", function () {
+	it("lockWithKey, should success", async () => {
+		let i = 0
+		await application._redis.lockWithKey('auto-key-test', async () => {
+			i++
+			console.log(1)
+			await application._redis.set('hello', 'world')
+			i++
+			console.log(2)
+			let value = await application._redis.get('hello')
+			i++
+			console.log(3)
+		})
+		i++
+		console.log(4, i)
+		assert( i === 4, 'lockWithKey fail')
+	})
+})
